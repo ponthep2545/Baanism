bd6d6326138f77c3eede2a73b5a19b1923c9d7e3
Pattima Sommai <ppp.pattima@hotmail.com>
2023-09-21 20:01:30 +0700

    finall nav bar >> responsive nav bar doesn't done

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './Header.css'
-import { func } from 'prop-types';
-// import './Header.css'; // Import your CSS file
-// import Hamburger from '../../assets/images/icon-hamburger.svg';
-// import Close from '../../assets/images/icon-close.svg';
 import logo from './../../assets/images/logo-header.png';
 import { Link } from 'react-router-dom';
 
@@ -20,16 +16,16 @@ export default function Header(){
         <img src={logo} alt="baanism-logo" className="headerLogo"/>
       </Link>
       <div className='headerMenu'>
-        <Link to="/about">
+        <Link to="/about" >
           เกี่ยวกับเรา
         </Link>
-        <Link to="/homecontent" className="space">
+        <Link to="/homecontent" >
           Home GURU Content
         </Link>
-        <Link to="/contact" className="space">
+        <Link to="/contact" >
           ประเมินราคา
         </Link>
-        <Link to="/contact" className="space">
+        <Link to="/contact" >
           ติดต่อเรา
         </Link>
       </div>
@@ -48,103 +44,3 @@ export default function Header(){
     </nav>
   );
 }
-
-// export default function Header() {
-//   const [navToggle, setNavToggle] = useState(false);
-
-//   const alternar = () => {
-//     setNavToggle(!navToggle);
-//   };
-
-//   useEffect(() => {
-//     if (navToggle === true) {
-//       document.body.style.overflow = 'hidden';
-//     } else {
-//       document.body.style.overflow = 'auto';
-//     }
-//   }, [navToggle]);
-
-//   return (
-//     <nav className="Container">
-//       <Link to="/">
-//         <img src={logo} alt="logo" className="Logo-header" />
-//       </Link>
-//       <div className={`MobileNav ${navToggle ? 'open' : ''}`}>
-//         <button onClick={alternar}>
-//           {navToggle ? (
-//             <i>
-//               <img src={Close} alt="Fechar" />
-//             </i>
-//           ) : (
-//             <i>
-//               <img src={Hamburger} alt="abrir" />
-//             </i>
-//           )}
-//         </button>
-//       </div>
-//       <div className={`header-container ${navToggle ? 'open' : ''}`}>
-//         <div className='header-menu'>
-//             <Link to="/about" >
-//               เกี่ยวกับเรา
-//             </Link>
-//             <Link to="/homecontent" className="space">
-//               Home GURU Content
-//           </Link>
-//           <Link to="/contact" className="space">
-//             ประเมินราคา
-//           </Link>
-//           <Link to="/contact" className="space">
-//             ติดต่อเรา
-//           </Link>
-//           <Link to="/login">
-//             <button className="login-button">
-//               ลงชื่อเข้าใช้งาน
-//             </button>
-//           </Link>
-//           <Link to="/login">
-//             <button className="register-button">
-//               ลงทะเบียน
-//             </button>
-//           </Link>
-//           {/* <button className="login-button">
-//               <Link to="/login"  >
-//                 ลงชื่อเข้าใช้งาน
-//               </Link>
-//             </button>
-          
-//             <button className="register-button">
-//               <Link to="/login" >
-//                 ลงทะเบียน
-//               </Link>
-//             </button> */}
-//         </div>
-
-//         {/* <div className="header-grap">
-//           <Link to="/about" className="space">
-//             เกี่ยวกับเรา
-//           </Link>
-//           <Link to="/homecontent" className="space">
-//             Home GURU Content
-//           </Link>
-//           <Link to="/contact" className="space">
-//             ประเมินราคา
-//           </Link>
-//           <Link to="/contact" className="space">
-//             ติดต่อเรา
-//           </Link>
-//           <button className="login-button">
-//             <Link to="/login"  >
-//               ลงชื่อเข้าใช้งาน
-//             </Link>
-//           </button>
-        
-//           <button className="register-button">
-//             <Link to="/login" >
-//               ลงทะเบียน
-//             </Link>
-//           </button>
-//         </div> */}
-//       </div>
-//     </nav>
-//   );
-// }
