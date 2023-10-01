704fe42b5089253a178dc5452c90396211970388
Pattima Sommai <ppp.pattima@hotmail.com>
2023-10-02 05:43:34 +0700

    add react-ucon bersion to package,package-lock
    add responsive nav bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,30 +1,60 @@
 import logo from './../../assets/images/logo-header.png';
-import './Header.css'
-import { Link, Routes, Route, useNavigate } from 'react-router-dom';
-import React, { useState, useEffect } from 'react'; // Import the React library
+import './Header.css';
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
-export default function Header(){
+export default function Header() {
   const navigate = useNavigate();
-  
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   const btnRegister = () => {
     navigate('/register');
-  }
+  };
 
   const btnLogin = () => {
     navigate('/login');
-  }
-  
-  return(
+  };
+
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 769) {
+        closeNav(); // Close the navigation when the screen size is 769px or more
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
     <>
-      <nav>
+      <nav className={`navbar ${isNavOpen ? 'active' : ''}`}>
         <Link to="/">
-          <img src={logo} alt="baanism-logo" className="logoStyle"/>
+          <img src={logo} alt="baanism-logo" className="logoStyle" />
         </Link>
 
-        <div>
+        <div className="nav-toggle" onClick={toggleNav}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+
+        <div className={`nav-links ${isNavOpen ? 'open' : ''}`}>
           <ul className='navBar'>
             <li>
-              <Link to="/about" >
+              <Link to="/about">
                 เกี่ยวกับเรา
               </Link>
             </li>
