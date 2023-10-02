7180c3c53abba20420844afde452e37b53dd792a
Pattima Sommai <ppp.pattima@hotmail.com>
2023-10-02 14:23:44 +0700

    add new logo navbar, edit color navbar, del note

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import logo from './../../assets/images/logo-header.png';
+import logo from './../../assets/images/LOGO_Linear_BGW_FTB.png';
 import './Header.css';
 import { Link, useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
